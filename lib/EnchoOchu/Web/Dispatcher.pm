07833af27b8e6b057f6a0116004dc6ed5e425ab4
sugyan <sugi1982@gmail.com>
2014-05-08 22:25:16 +0900

    update top page

--- lib/EnchoOchu/Web/Dispatcher.pm
+++ lib/EnchoOchu/Web/Dispatcher.pm
@@ -6,18 +6,8 @@ use Amon2::Web::Dispatcher::RouterBoom;
 
 any '/' => sub {
     my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
     return $c->render('index.tx', {
-        counter => $counter,
     });
-};
-
-post '/reset_counter' => sub {
-    my $c = shift;
-    $c->session->remove('counter');
-    return $c->redirect('/');
 };
 
 post '/account/logout' => sub {
