5fb2ff1ff4ecec550ceb1a3504f7790ac7547c41
sugyan <sugi1982@gmail.com>
2014-05-13 17:42:49 +0900

    update web

--- lib/EnchoOchu/Web/Dispatcher.pm
+++ lib/EnchoOchu/Web/Dispatcher.pm
@@ -6,12 +6,21 @@ use Amon2::Web::Dispatcher::RouterBoom;
 
 any '/' => sub {
     my ($c) = @_;
+    my $count = $c->db->count('member' => '*');
     return $c->render('index.tx', {
+        count => $count,
     });
 };
 
 get '/account/logout' => sub {
     my ($c) = @_;
+    $c->session->expire();
+    return $c->redirect('/');
+};
+
+post '/account/delete' => sub {
+    my ($c) = @_;
+    $c->user->delete();
     $c->session->expire();
     return $c->redirect('/');
 };
